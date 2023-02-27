47e2fa507c25a14df9d138b4496a6dd3bc6ae01c
MikhalevaSophia <112760887+MikhalevaSophia@users.noreply.github.com>
2023-02-27 12:23:12 -0500

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@ include <iostream>
 include <string>;
 int main () {
 	std::string name;
-	// Бабушка технарь, дед гуманитарий
+	
 	std::cout << "Please enter name";
 	std::cin >> name;
 	std::cout << "Hello world from " << name;
