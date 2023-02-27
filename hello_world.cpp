f6427e2d2178be3d653e075956b517431a017e68
MikhalevaSofia <MixSo.net@yandex.ru>
2023-02-27 11:29:30 -0500

    added comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,9 @@ include <iostream>
 include <string>;
 int main () {
 	std::string name;
+	// Бабушка технарь, дед гуманитарий
 	std::cout << "Please enter name";
 	std::cin >> name;
 	std::cout << "Hello world from " << name;
+
 }
